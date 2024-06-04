cad10ab920a5e95aab0cf629ad6072c403e5b6cb
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-04 13:13:06 +0100

    Created a new component file This will be used to display an output on the HTML page.

---
+++ src/Component.jsx
@@ -0,0 +1,13 @@
+function Batch(){
+    //logic
+    const a =5
+    const b =5
+    const c =a*b
+    return(
+        <div><h1>Batch Component</h1>
+        <p>{c}</p>
+        </div>
+    )
+}
+
+export default Batch                
