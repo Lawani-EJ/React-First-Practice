d290e4f191be6eebd80899495fa9287cb38806f5
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-04 13:13:19 +0100

    Created a new component file This will be used to display an output on the HTML page.

---
+++ src/Component2.jsx
@@ -0,0 +1,11 @@
+function Hello(){
+    const name = "Elyon"
+    const l_name = "John"
+    const fname = name +  " " +  l_name
+
+    return(
+        <div><h3 className="b">Hello {fname}</h3></div>
+    )
+}
+
+export default Hello
