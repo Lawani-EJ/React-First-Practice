98879fa15f7b40f3e7988ebe29566babba76302f
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-04 13:08:37 +0100

    New Mods on The App Js file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import './style2.css'
+import Batch from './Component';
+import Hello from './Component2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <p className='a'>Working on it</p>
+      <Batch />
+      <Hello />
     </div>
   );
 }
